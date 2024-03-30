9b058208a733051599bc8f4cd9bcabb69c3a7901
zishan344 <zishanahmed344@gmail.com>
2024-03-31 04:50:01 +0600

    first commit

---
+++ print.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+/*     write first c program
+   printf("hello world\thi\\"); */
+  // tr to practice data type
+  // int test
+/*   int testInt = 1;
+  printf("%d", testInt); */
+  // float test
+  /* float testFloat = 2.2;
+  printf("%.2f", testFloat); */
+  char charTest = 'a';
+  printf("%c",charTest);
+  return 0;
+};
