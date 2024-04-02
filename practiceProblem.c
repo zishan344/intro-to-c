e95c85f00adaa32a4795d9d9a7bda9a1e5c63be1
zishan344 <zishanahmed344@gmail.com>
2024-04-02 21:08:08 +0600

    problem 1 completed

---
+++ practiceProblem.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void)
+{
+  printf("Recently I heard that you’ve achieved 95%% marks in your exam.\n This is brilliant!\n I wish you’ll shine in your life!    Good luck with all the barriers(/\\) in your life.");
+};
