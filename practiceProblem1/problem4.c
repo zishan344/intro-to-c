1e031e89f0b8fbc69ec2b8ce247c3aef20653a38
zishan344 <zishanahmed344@gmail.com>
2024-04-02 22:24:14 +0600

    complete problem 4

---
+++ practiceProblem1/problem4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+  int a;
+  scanf("%d", &a);
+  if(a > 0){
+    printf("positive");
+
+  } else if(a < 0){
+
+    printf("negative");
+  }else{
+
+    printf("zero");
+  };
+};
