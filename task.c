a19d838c824753a25f5bf8c63850abef667456ea
zishan344 <zishanahmed344@gmail.com>
2024-04-05 04:01:37 +0600

    ass_p5_done

--- task.c
+++ task.c
@@ -12,8 +12,15 @@ int main (){
  */
 
 //problem 3
-
-    int n;
+/*  int a;
+    scanf("%d",&a);
+    if(a%3==0){
+        printf("YES");
+    }else{
+        printf("NO");
+    } */
+   //4 
+  /*   int n;
     int i;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
@@ -26,9 +33,19 @@ int main (){
         }
             
          
+    }; */
+
+//problem 5
+ int a;
+    scanf("%d", &a);
+    if(a>=1500){
+        printf("I will buy Punjabi\nI will buy new shoes\nAlisa will buy new shoes");
+    }else if(a>1000)
+    {
+          printf("I will buy Punjabi");
+    }else {
+         printf("Bad luck!");
     };
-
-
  
   return 0;
  };
