7e6642a2c27b55e1d1c47aa558915acce4ad8f1c
zishan344 <zishanahmed344@gmail.com>
2024-04-05 03:23:34 +0600

    ass_p2_done

--- task.c
+++ task.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 int main (){
  
-printf("Hello, world! I am learning C programming language. ^_^\nProgramming is fun and challenging. /\\/\\/\\\nI want to give my 100%% dedication to learn!\tI will succeed one day.");
+//  problem 1
+/* printf("Hello, world! I am learning C programming language. ^_^\nProgramming is fun and challenging. /\\/\\/\\\nI want to give my 100%% dedication to learn!\tI will succeed one day."); */
+// problem 2
+/* 
+    long long a ;
+    long long b ;
+    scanf("%lld %lld", &a,&b);
+    printf("%lld", a*b);
+ */
 
- 
  
   return 0;
  };
